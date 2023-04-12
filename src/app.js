b479d802473cc1d14fc3a573c967f582f8ae3edf
sahil thakur <sahilthakurxyz@gmail.com>
2023-04-13 01:29:09 +0530

    hello newton just away from me hahaha

--- src/app.js
+++ src/app.js
@@ -1,19 +1,39 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.method === "POST") {
+  if (req.method === "GET") {
     const chunks = [];
 
-    req.on("data", (chunk) => {
+    req.on(" data", (chunk) => {
       const buf = Buffer.from(chunk);
       const str = buf.toString();
       chunks.push(str);
 
       const obj = JSON.parse(chunks);
       const value = obj.num1;
+      if (!value || Number.isNaN(value)) {
+        res.statusCode = 400;
+        res.end("Bad request");
+        return;
+      }
+      if (value % 2 == 0) {
+        res.statusCode = 200;
+        res.end(`The number  ${value} is even `);
+      } else {
+        res.statusCode = 400;
+        res.end(`The number ${value} is odd`);
+      }
+    });
+  }
+});
+
+module.exports = server;
+/*
+ 
 
       // Write the code here to check if the number is odd or even
       if (Number(value) % 2 === 0) {
+        console.log(Number(value));
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/plain");
         res.end("The number is even");
@@ -22,11 +42,5 @@ const server = http.createServer((req, res) => {
         res.setHeader("Content-Type", "text/plain");
         res.end("The number is odd");
       }
-    });
-  } else {
-    res.statusCode = 404;
-    res.end("Not Found");
-  }
-});
-
-module.exports = server;
+   
+*/
