1b30115167225e87487dec10b26819b1d0984ccc
sahil thakur <sahilthakurxyz@gmail.com>
2023-04-13 14:22:57 +0530

    hello newton just away from me hahahahg

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.method === "GET") {
+  if (req.method === "POST") {
     const chunks = [];
 
-    req.on(" data", (chunk) => {
+    req.on("data", (chunk) => {
       const buf = Buffer.from(chunk);
       const str = buf.toString();
       chunks.push(str);
